4d19f9eab8f60f0d55b4b7bcdb29f5b1da9a0dbd
Service <info@service.money>
2025-07-10 09:33:28 -0400

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/HVAC-Finance-Dashboard/', // 👈 This must match your repo name
-});
