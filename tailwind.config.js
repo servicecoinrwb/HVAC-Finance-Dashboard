3a87aed11f938a843ccf9cfb5f410d3539802002
Service <info@service.money>
2025-07-10 18:55:57 -0400

    Fix build error with icon import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', // <-- Add this line
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  //... rest of the file
 }
